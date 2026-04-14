3283c2001a6c2205d9aecec7d6f0ff717579c499
AbdulRauf Siddiqui <abdul.rauf@toko.pk>
2026-04-14 06:48:34 +0500

    bug fix

--- voice_orchestrator/asgi.py
+++ voice_orchestrator/asgi.py
@@ -8,13 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voice_orchestrator.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import websocket.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voice_orchestrator.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
